65ac82e3f4050a9c038d0b5408ff93f0aa3c2985
Yuliya Payevska <petrikjul@gmail.com>
2018-04-02 21:57:00 +0300

    add slider in section-5

---
+++ js/script.js
@@ -0,0 +1,34 @@
+$(document).ready(function(){
+	$('.next').click(function() {
+		var currentImage = $( '.img.curry' );
+		var currentImageIndex = $( '.img.curry' ).index();
+		var nextImageIndex = currentImageIndex + 1;
+		var nextImage = $( '.img' ).eq(nextImageIndex);
+
+		$( '.img' ).eq(currentImageIndex).hide();
+		$( '.img' ).eq(currentImageIndex).removeClass('curry');
+
+		if(nextImageIndex == ($( '.img:last' ).index() + 1)){
+			$( '.img' ).eq(0).css('display', 'flex');
+			$( '.img' ).eq(0).addClass('curry');
+		} else {
+			$( '.img' ).eq(nextImageIndex).css('display', 'flex');
+			nextImage.addClass('curry');
+		}
+	});
+
+	$('.prev').click(function() {
+		var currentImage = $( '.img.curry' );
+		var currentImageIndex = $( '.img.curry' ).index();
+		var prevImageIndex = currentImageIndex - 1;
+		var prevImage = $( '.img' ).eq(prevImageIndex);
+
+		currentImage.hide();
+		currentImage.removeClass('curry');
+
+		prevImage.css('display', 'flex');
+		prevImage.addClass('curry');
+
+	});
+
+});
